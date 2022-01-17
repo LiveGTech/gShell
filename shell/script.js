7ee3f8e716c6045b0e9bfcdcaacd863dc3a660c5
James-Livesey <jamesliveg@gmail.com>
2022-01-17 11:56:13 +0000

    Integrate Adapt UI into gShell

--- shell/script.js
+++ shell/script.js
@@ -7,32 +7,34 @@
     Licensed by the LiveG Open-Source Licence, which can be found at LICENCE.md.
 */
 
-window.addEventListener("load", function() {
-    document.querySelector("#shutDownButton").addEventListener("click", function() {
+import * as $g from "./lib/adaptui/src/adaptui.js";
+
+$g.waitForLoad().then(function() {
+    $g.sel("#shutDownButton").on("click", function() {
         gShell.call("power_shutDown");
     });
 
-    document.querySelector("#devRestartButton").addEventListener("click", function() {
+    $g.sel(".devRestartButton").on("click", function() {
         gShell.call("dev_restart");
     });
 
     gShell.call("system_getFlags").then(function(flags) {
         if (flags.isRealHardware) {
-            document.querySelector("#flagInfo").textContent = "Running on real hardware!";
+            $g.sel("#flagInfo").setText("Running on real hardware!");
         } else {
-            document.querySelector("#flagInfo").textContent = "Running in simulator!";
+            $g.sel("#flagInfo").setText("Running in simulator!");
         }
     });
 
     function checkStatus() {
         gShell.call("power_getState").then(function(response) {
             if (response.state == null) {
-                document.querySelector("#batteryStatus").textContent = "No battery detected";
+                $g.sel("#batteryStatus").setText("No battery detected");
     
                 return;
             }
     
-            document.querySelector("#batteryStatus").textContent = `Battery: ${response.state}, ${response.level}%`;
+            $g.sel("#batteryStatus").setText(`Battery: ${response.state}, ${response.level}%`);
         });
     }
 

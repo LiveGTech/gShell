df2012a9fc9b83ec9cfc7df16a10e6f12181d18f
James-Livesey <jamesliveg@gmail.com>
2022-02-13 14:48:51 +0000

    Add scrolling to switcher

--- shell/userenv/switcher.js
+++ shell/userenv/switcher.js
@@ -9,16 +9,22 @@
 
 import * as $g from "gshell://lib/adaptui/src/adaptui.js";
 
+const SELECT_MOTION_TOLERANCE = 20;
+
+var initialTouchX = 0;
+var initialScrollX = 0;
+var lastScrollX = 0;
 var targetScrollX = 0;
 var targetInstantaneous = false;
 var touchIsDown = false;
+var scrolling = false;
 
 function getClosestScreen() {
     var switcherWidth = $g.sel(".switcher").get().clientWidth;
     var switcherScrollLeft = $g.sel(".switcher").get().scrollLeft;
 
     return $g.sel(".switcher .switcher_screen").getAll().map(function(screenElement) {
-        if (Math.abs(screenElement.offsetLeft - switcherScrollLeft) < switcherWidth / 2) {
+        if (Math.abs(screenElement.offsetLeft - switcherScrollLeft) < (switcherWidth * (1 - 0.12)) / 2) { // 0.12 being margin percentage for switcher screens
             return $g.sel(screenElement);
         }
 
@@ -32,12 +38,77 @@ export function init() {
     });
 
     $g.sel(".switcher").on("click", function(event) {
+        if (scrolling || Math.abs(event.pageX - initialTouchX) > SELECT_MOTION_TOLERANCE) {
+            return;
+        }
+
         if (
             event.target.matches(".switcher_screen") &&
             event.target.closest(".switcher").matches(".allowSelect")
         ) {
             selectScreen($g.sel(event.target));
         }
+    });
+
+    $g.sel(".switcher").on("mousedown touchstart", function(event) {
+        if (scrolling) {
+            return;
+        }
+
+        initialTouchX = event.pageX;
+        initialScrollX = $g.sel(".switcher").get().scrollLeft;
+        touchIsDown = true;
+        scrolling = true;
+    });
+
+    $g.sel(".switcher").on("mousemove touchmove", function(event) {
+        if (!touchIsDown) {
+            return;
+        }
+
+        var switcherElement = $g.sel(".switcher").get();
+
+        lastScrollX = switcherElement.scrollLeft;
+        switcherElement.scrollLeft = initialScrollX - (event.pageX - initialTouchX);
+    });
+
+    $g.sel(".switcher").on("mouseup touchend", function(event) {
+        var switcherElement = $g.sel(".switcher").get();
+
+        function snapScrolling() {
+            var closestScreen = getClosestScreen();
+    
+            if (closestScreen != null) {
+                targetScrollX = closestScreen.get().offsetLeft;
+            }
+
+            scrolling = false;
+        }
+
+        var rate = switcherElement.scrollLeft - lastScrollX;
+        var multiplier = 1;
+
+        touchIsDown = false;
+
+        if (Math.abs(event.pageX - initialTouchX) <= SELECT_MOTION_TOLERANCE) {
+            snapScrolling();
+
+            return;
+        }
+
+        requestAnimationFrame(function continueScrolling() {
+            switcherElement.scrollLeft += rate * multiplier;
+
+            multiplier *= 0.9;
+
+            if (multiplier > 0.1) {
+                requestAnimationFrame(continueScrolling);
+
+                return;
+            }
+
+            snapScrolling();
+        });
     });
 
     window.addEventListener("resize", function() {
@@ -50,7 +121,7 @@ export function init() {
     });
 
     setInterval(function() {
-        if (!touchIsDown) {
+        if (!scrolling) {
             var element = $g.sel(".switcher").get();
             var change = (targetScrollX - element.scrollLeft) * 0.2;
 

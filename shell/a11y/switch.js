594a09a7bf6122a6e4e1ab5509dfba97612f0a2d
James-Livesey <jamesliveg@gmail.com>
2022-04-01 18:30:01 +0100

    Add basic switch a11y menu implementation

--- shell/a11y/switch.js
+++ shell/a11y/switch.js
@@ -8,24 +8,41 @@
 */
 
 import * as $g from "gshell://lib/adaptui/src/adaptui.js";
+import * as aui_a11y from "gshell://lib/adaptui/src/a11y.js";
 
 import * as a11y from "gshell://a11y/a11y.js";
 import * as input from "gshell://input/input.js";
 import * as webviewComms from "gshell://userenv/webviewcomms.js";
+
+export var menus = {
+    NONE: 0,
+    MAIN: 1
+};
 
 export class SwitchNavigation extends a11y.AssistiveTechnology {
     constructor() {
         super();
 
         this.itemScanNextAt = -Infinity;
+        this._currentMenu = menus.NONE;
+    }
+
+    get currentMenu() {
+        return this._currentMenu;
+    }
+
+    set currentMenu(value) {
+        this._currentMenu = value;
+
+        this.renderMenu();
     }
 
     init() {
         var thisScope = this;
 
         function keydownCallback(event) {
-            switch (event.code) {
-                case "Space":
+            switch (event.key) {
+                case " ":
                     thisScope.itemScanNextAt = Date.now() + a11y.options.switch_itemScanAfterConfirmPeriod;
                     break;
             }
@@ -33,6 +50,10 @@ export class SwitchNavigation extends a11y.AssistiveTechnology {
 
         $g.sel("body").on("keydown", keydownCallback);
         webviewComms.onEvent("keydown", keydownCallback);
+
+        $g.sel(".a11y_switch_openMainMenuButton").on("click", function() {
+            thisScope.currentMenu = menus.MAIN;
+        });
 
         setInterval(function() {
             if (!a11y.options.switch_enabled) {
@@ -55,6 +76,88 @@ export class SwitchNavigation extends a11y.AssistiveTechnology {
     update() {
         $g.sel("body").setAttribute("liveg-a11y-switch", a11y.options.switch_enabled);
         $g.sel("body").setAttribute("liveg-a11y-scan", a11y.options.switch_enabled ? a11y.options.switch_scanColour : "");
+    }
+
+    renderMenu() {
+        var thisScope = this;
+
+        if (this.currentMenu == menus.NONE) {
+            $g.sel(".a11y_switch_menu").hide();
+
+            aui_a11y.clearFocusTrap();
+
+            return;
+        }
+
+        function createMenuItem(text, icon, action = function() {}) {
+            return $g.create("button")
+                .setAttribute("aria-label", text)
+                .on("click", function() {
+                    thisScope.currentMenu = menus.NONE;
+
+                    action();
+                })
+                .add(
+                    $g.create("img")
+                        .setAttribute("aui-icon", "dark embedded")
+                        .setAttribute("src", `gshell://lib/adaptui/icons/${icon}.svg`)
+                    ,
+                    $g.create("span").setText(text)
+                )
+            ;
+        }
+
+        function setMenuItems(items) {
+            $g.sel(".a11y_switch_menu").clear();
+
+            var row = $g.create("div");
+
+            for (var i = 0; i < items.length; i++) {
+                row.add(items[i]);
+
+                if (i % 4 == 3) {
+                    $g.sel(".a11y_switch_menu").add(row);
+
+                    row = $g.create("div");
+                }
+            }
+
+            $g.sel(".a11y_switch_menu").add(row);
+        }
+
+        switch (this._currentMenu) {
+            case menus.MAIN:
+                setMenuItems([
+                    createMenuItem(_("a11y_switch_switchToPointScan"), "star", function() {
+                        console.log("Hi!");
+                    }),
+                    createMenuItem(_("a11y_switch_switchToPointScan"), "star", function() {
+                        console.log("Hi!");
+                    }),
+                    createMenuItem(_("a11y_switch_switchToPointScan"), "star", function() {
+                        console.log("Hi!");
+                    }),
+                    createMenuItem(_("a11y_switch_switchToPointScan"), "star", function() {
+                        console.log("Hi!");
+                    }),
+                    createMenuItem(_("a11y_switch_switchToPointScan"), "star", function() {
+                        console.log("Hi!");
+                    })
+                ]);
+
+                break;
+
+            default:
+                setMenuItems([]);
+
+                break;
+        }
+
+        $g.sel(".a11y_switch_menu").show();
+
+        $g.sel(".a11y_switch_menu button:first-of-type").focus();
+
+        aui_a11y.setFocusTrap($g.sel(".a11y_switch_menu").get());
     }
 }
 

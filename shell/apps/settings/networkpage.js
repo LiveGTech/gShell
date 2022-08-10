9081f108f54c5ba46393af4b75dd662755d98739
James-Livesey <jamesliveg@gmail.com>
2022-08-10 18:41:27 +0100

    Add no Wi-Fi networks message to Settings

--- shell/apps/settings/networkpage.js
+++ shell/apps/settings/networkpage.js
@@ -17,57 +17,66 @@ export var NetworkPage = astronaut.component("NetworkPage", function(props, chil
     function updateData() {
         var data = _sphere.getPrivilegedData();
 
-        wifiScanResultsContainer.clear().add(
-            ...(data?.network_wifiScanResults || [])
-                .filter((result) => result.name != "")
-                .map(function(result) {
-                    var securityType = "unknown";
+        if (data?.network_wifiScanResults?.length > 0) {
+            wifiScanResultsContainer.clear().add(
+                ...(data?.network_wifiScanResults || [])
+                    .filter((result) => result.name != "")
+                    .map(function(result) {
+                        var securityType = "unknown";
 
-                    if (result.security.length == 0) {
-                        securityType = "open";
-                    } else if (result.security.includes("wpa2")) {
-                        securityType = "wpa2";
-                    } else if (result.security.includes("wpa1")) {
-                        securityType = "wpa1";
-                    } else if (result.security.includes("wep")) {
-                        securityType = "wep";
-                    }
+                        if (result.security.length == 0) {
+                            securityType = "open";
+                        } else if (result.security.includes("wpa2")) {
+                            securityType = "wpa2";
+                        } else if (result.security.includes("wpa1")) {
+                            securityType = "wpa1";
+                        } else if (result.security.includes("wep")) {
+                            securityType = "wep";
+                        }
 
-                    var details = [];
+                        var details = [];
 
-                    if ((data?.network_listResults || []).filter((result) => result.connected)[0]?.name == result.name) {
-                        details.push(_("network_connected"));
-                    }
+                        if ((data?.network_listResults || []).filter((result) => result.connected)[0]?.name == result.name) {
+                            details.push(_("network_connected"));
+                        }
 
-                    if (securityType == "open") {
-                        details.push(_("network_wifiSecurityType_open"));
-                    } else if (securityType == "unknown") {
-                        details.push(_("network_wifiSecurityType_unknown"));
-                    } else {
-                        details.push(_("network_wifiSecurityType_secured"));
-                    }
+                        if (securityType == "open") {
+                            details.push(_("network_wifiSecurityType_open"));
+                        } else if (securityType == "unknown") {
+                            details.push(_("network_wifiSecurityType_unknown"));
+                        } else {
+                            details.push(_("network_wifiSecurityType_secured"));
+                        }
 
-                    var button = IconListButton() (
-                        Icon(`wifi-${Math.round((result.signal / 100) * 2)}`, "dark embedded") (),
-                        Container (
-                            BoldTextFragment() (result.name),
-                            LineBreak() (),
-                            Text(_("network_wifiScanResultDetails", {
-                                details: details.join(" · "),
-                                bandwidth: result.bandwidth
-                            }))
-                        )
-                    );
-
-                    button.on("click", function() {
-                        settings.visitInnerScreen(
-                            WifiApScreen({accessPoint: result}) ()
+                        var button = IconListButton() (
+                            Icon(`wifi-${Math.round((result.signal / 100) * 2)}`, "dark embedded") (),
+                            Container (
+                                BoldTextFragment() (result.name),
+                                LineBreak() (),
+                                Text(_("network_wifiScanResultDetails", {
+                                    details: details.join(" · "),
+                                    bandwidth: result.bandwidth
+                                }))
+                            )
                         );
-                    });
 
-                    return button;
-                })
-        );
+                        button.on("click", function() {
+                            settings.visitInnerScreen(
+                                WifiApScreen({accessPoint: result}) ()
+                            );
+                        });
+
+                        return button;
+                    })
+            );
+        } else {
+            wifiScanResultsContainer.clear().add(
+                Message (
+                    Heading(3) (_("network_wifiNetworksNone_title")),
+                    Paragraph() (_("network_wifiNetworksNone_description"))
+                )
+            );
+        }
     }
 
     _sphere.onPrivilegedDataUpdate(updateData);

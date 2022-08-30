9d302abf2dffbe98a3318b97a82a222e3d10d146
James-Livesey <jamesliveg@gmail.com>
2022-08-30 16:21:17 +0100

    Fix button styling for pagination buttons

--- shell/userenv/home.js
+++ shell/userenv/home.js
@@ -110,6 +110,8 @@ export function load() {
                         )
                 );
             });
+
+            scroller.applyPagination($g.sel(".home_pagination"));
         });
     });
 }

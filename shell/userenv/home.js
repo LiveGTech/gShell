0ff3ced69721686394f053f9bc8ab1994e2e03a0
James-Livesey <jamesliveg@gmail.com>
2023-02-05 15:46:50 +0000

    Add app menu alphabetical view

--- shell/userenv/home.js
+++ shell/userenv/home.js
@@ -38,6 +38,36 @@ export function init() {
         });
     });
 
+    $g.sel(".home_gridViewButton").on("click", function() {
+        if (!$g.sel(".home.home_appMenuTab").hasAttribute("hidden")) {
+            return;
+        }
+
+        $g.sel(".desktop_appMenuSide button").removeClass("selected");
+        $g.sel(".home_gridViewButton").addClass("selected");
+
+        Promise.all($g.sel(".home_appMenuTab", true).fadeOut(250)).then(function() {
+            $g.sel(".desktop_appMenuLayout").addClass("gridViewSelected");
+
+            $g.sel(".home.home_appMenuTab").fadeIn(250);
+        });
+    });
+
+    $g.sel(".home_alphabeticalViewButton").on("click", function() {
+        if (!$g.sel(".home_alphabeticalView.home_appMenuTab").hasAttribute("hidden")) {
+            return;
+        }
+
+        $g.sel(".desktop_appMenuSide button").removeClass("selected");
+        $g.sel(".home_alphabeticalViewButton").addClass("selected");
+
+        Promise.all($g.sel(".home_appMenuTab", true).fadeOut(250)).then(function() {
+            $g.sel(".desktop_appMenuLayout").removeClass("gridViewSelected");
+
+            $g.sel(".home_alphabeticalView.home_appMenuTab").fadeIn(250);
+        });
+    });
+
     users.onUserStateChange(function(user) {
         if (user != null) {
             load();
@@ -92,17 +122,50 @@ export function load() {
 
             $g.sel(".home").getAll().forEach(function(homeElement) {
                 homeElement = $g.sel(homeElement);
-        
-                homeElement.clear().add(
-                    $g.create("div")
-                        .addClass("home_page")
-                        .add(
-                            ...Object.values(configData.apps).map((app) => createApp({
-                                ...app,
-                                name: app.name[l10n.currentLocale.localeCode] || app.name[app.fallbackLocale]
-                            }))
-                        )
-                );
+
+                homeElement.clear();
+
+                $g.sel(".home_alphabeticalView").clear();
+
+                Object.values(configData.apps).forEach(function(app) {
+                    var appElement = createApp({
+                        ...app,
+                        name: app.name[l10n.currentLocale.localeCode] || app.name[app.fallbackLocale]
+                    });
+
+                    var firstFreePage = homeElement.find(".home_page").filter((page) => page.find(".home_app").items().length < 24);
+
+                    if (firstFreePage.items().length == 0) {
+                        firstFreePage = $g.create("div").addClass("home_page");
+
+                        homeElement.add(firstFreePage);
+                    }
+
+                    firstFreePage.add(appElement);
+                });
+            });
+
+            $g.sel(".home_alphabeticalView").getAll().forEach(function(homeElement) {
+                homeElement = $g.sel(homeElement);
+
+                homeElement.clear();
+
+                Object.values(configData.apps)
+                    .map(function(app) {
+                        app.displayName = app.name[l10n.currentLocale.localeCode] || app.name[app.fallbackLocale];
+
+                        return app;
+                    })
+                    .sort((a, b) => l10n.currentLocale.createCollator().compare(a.displayName, b.displayName))
+                    .forEach(function(app) {
+                        var appElement = createApp({
+                            ...app,
+                            name: app.displayName
+                        });
+
+                        homeElement.add(appElement);
+                    })
+                ;
             });
 
             scrollers.forEach(function(scroller, i) {

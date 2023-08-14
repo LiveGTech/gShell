8b10c477fa8917d998863635552533695092a554
James-Livesey <jamesliveg@gmail.com>
2023-08-14 19:21:13 +0100

    Bump version to V0.3.0

--- shell/about.js
+++ shell/about.js
@@ -7,5 +7,5 @@
     Licensed by the LiveG Open-Source Licence, which can be found at LICENCE.md.
 */
 
-export const VERSION = "0.2.0";
-export const VERNUM = 1;
+export const VERSION = "0.3.0";
+export const VERNUM = 2;
